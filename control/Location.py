3cfc1f429794aadf60ba1b38f16651a1853edd1b
larshof1997 <88612524+larshof1997@users.noreply.github.com>
2024-05-20 16:05:25 +0200

    fixed polygon

--- control/Location.py
+++ control/Location.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon, MultiLineString, MultiPolygon, LineString
 import os
 import numpy as np
+import itertools
+import networkx as nx
+import math
 
 class Map:
     def __init__(self, position, osm_file):
@@ -18,18 +21,8 @@ class Map:
         nodes, edges = ox.graph_to_gdfs(G, fill_edge_geometry=True)
         self.graph = ox.graph_from_gdfs(nodes, edges, graph_attrs=G.graph)
 
-        # self.features = ox.features_from_xml(osm_file)
-
         # Extract the lines from the graph as a GeoDataFrame
         self.lines = edges
-
-        # self.lines = self.features[self.features['type']=='line_thin']
-        # self.graph = ox.graph_from_gdfs(self.features, nodes=True, edges=False)
-        
-        # print(lines['geometry'])
-
-        # # Extract the edges (roads) from the graph as a GeoDataFrame
-        # self.edges_gdf = ox.graph_to_gdfs(self.graph, nodes=False, edges=True)
 
         # Preprocess the edges GeoDataFrame
         self.preprocess_edges()
@@ -44,82 +37,28 @@ class Map:
         for i, line_string in enumerate(line_strings):
             if i+1 in [6,8,11, 12, 14]:
                 x, y = line_string.xy
-                plt.plot(x, y, label=f'Line {i+1}')
+                # plt.plot(x, y, label=f'Line {i+1}')
                 lines[f"{i+1}"] = list(line_string.coords)
-                print(list(line_string.coords))
         concatenated_line_string = [
             list(reversed(list(lines["11"])))+
-            list(lines["12"]) +
-            list(reversed(list(lines["14"]))) +
+            list(reversed(list(lines["12"]))) +
+            list((lines["14"])) +
             list(reversed(list(lines["6"]))) +
-            list(lines["8"]) 
+            list(lines["8"]) +
+            list(lines["6"])
         ]
-
 
         polypoints = []
         for tuple in concatenated_line_string:
-            print(tuple)
             for point in tuple:
-                print(point)
                 polypoints.append([point[0],point[1]])
-
-        print(polypoints)
-        
-        # Concatenate LineStrings in the desired order to form the polygon boundary
-        concatenated_line_string = LineString(
-            lines["11"] +
-            lines["14"] +
-            list(reversed(lines["12"])) +
-            list(reversed(lines["6"])) +
-            lines["8"]
-        )
-
-        # Plot the concatenated LineString to visualize the boundary
-        plt.plot(*concatenated_line_string.xy, label='Polygon Boundary', color='black')
-
-        # Show legend
-        plt.legend()
 
         # Convert the concatenated LineString to a Polygon
         self.polygon = Polygon(polypoints)
-        plt.legend()  # Add legend
-        plt.show()
-        # # Create a polygon from all coordinates
-        # self.polygon = Polygon(concatenated_line_string)
-        # self.polygon = Polygon(polypoints)
-        # self.polygon = Polygon(lines)
-
-        # self.polygon =Polygon(
-        # [
-        #     [10.470650724002098, 55.38520996875883],
-        #     [10.469291130631092, 55.38477524425895],
-        #     [10.468346742421403, 55.3855938172706],
-        #     [10.466164880005067, 55.384530131529345],
-        #     [10.46587993528641, 55.38429426690277],
-        #     [10.46378762692467, 55.381052141937516],
-        #     [10.46446335297145, 55.38091801121436],
-        #     [10.465041383686241, 55.380941137233464],
-        #     [10.465733392288826, 55.3808902599732],
-        #     [10.466262575337083, 55.3807653791485],
-        #     [10.467736146595996, 55.38083475743292],
-        #     [10.469681911959213, 55.38064974840299],
-        #     [10.470015704344348, 55.381177021857354],
-        #     [10.4717823615984, 55.38068212504581],
-        #     [10.472189425482355, 55.380964263239036],
-        #     [10.473744409517451, 55.38081625656886],
-        #     [10.470553028670054, 55.381861541821166],
-        #     [10.469535368961601, 55.38115852115337],
-        #     [10.467695440207848, 55.381491532500746],
-        #     [10.469193435299047, 55.38253217489091],
-        #     [10.467003431605235, 55.3834109182535],
-        #     [10.468371166253775, 55.384391387802054],
-        #     [10.47115548321807, 55.38393815489968],
-        #     [10.471619536045012, 55.384285017281144],
-        #     [10.470650724002098, 55.38520996875883],
-        # ]
-        # )
-        
-
+        # plt.legend()
+        # plt.legend()  # Add legend
+        # plt.show()
+    
 
     def update_location(self, position):
         if self.check_point_in_polygon(position):
@@ -155,23 +94,148 @@ class Map:
         plt.legend()
         plt.show()
 
+    def discretize_polygon(self, step_size=0.00001):
+        min_x, min_y, max_x, max_y = self.polygon.bounds
+        x_coords = np.arange(min_x, max_x, step_size)
+        y_coords = np.arange(min_y, max_y, step_size)
+        self.grid = [Point(x, y) for x, y in itertools.product(x_coords, y_coords) if self.polygon.contains(Point(x, y))]
+
+    def plot_discretized_points(self):
+        fig, ax = plt.subplots()
+        x, y = self.polygon.exterior.xy
+        plt.plot(x, y, label='Polygon')
+        
+        points_coords = np.array([(point.x, point.y) for point in self.grid])
+        plt.scatter(points_coords[:, 0], points_coords[:, 1], color='blue', s=1, label='Discretized Points')
+
+        plt.title('Discretized Points within Polygon')
+        plt.xlabel('Longitude')
+        plt.ylabel('Latitude')
+        plt.legend()
+        plt.show()
+
+    def check_steering_angle(self, p1, p2, max_angle=35):
+        dx = p2.x - p1.x
+        dy = p2.y - p1.y
+        angle = np.arctan2(dy, dx) * 180 / np.pi
+        return abs(angle) <= max_angle
+
+    def create_edges(self, max_angle=45):
+        self.edges = []
+        for p1, p2 in itertools.combinations(self.grid, 2):
+            if self.check_steering_angle(p1, p2, max_angle):
+                self.edges.append((p1, p2))
+
+    def build_graph_from_points(self):
+        self.G = nx.Graph()
+        for point in self.grid:
+            self.G.add_node((point.x, point.y))
+        for edge in self.edges:
+            p1, p2 = edge
+            self.G.add_edge((p1.x, p1.y), (p2.x, p2.y))
+
+    def euclidean_distance(self, x1, y1, x2, y2):
+        """
+        Calculate the Euclidean distance between two points
+        """
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    def closest_node(self, lat, lon, nodes):
+        """
+        Find the closest node to a given latitude and longitude point
+        from a list of nodes.
+        """
+        closest_node = None
+        min_distance = float('inf')
+
+        for node in nodes:
+            distance = self.euclidean_distance(lon, lat, node[0], node[1])
+            if distance < min_distance:
+                min_distance = distance
+                closest_node = node
+
+        return closest_node
+
+    def find_shortest_path(self, start, end):
+        self.discretize_polygon()
+        self.create_edges()
+        self.build_graph_from_points()
+
+        start_node = self.closest_node(start[0], start[1], self.G.nodes)
+        end_node = self.closest_node(end[0], end[1],self.G.nodes)
+        print(start_node,end_node)
+
+        shortest_path = nx.shortest_path(self.G, start_node, end_node)
+
+        path_coords = [(x, y) for x, y in shortest_path]
+        return path_coords
+
+    def plot_path(self, path_coords):
+        fig, ax = plt.subplots()
+        x, y = self.polygon.exterior.xy
+        plt.plot(x, y, label='Polygon')
+        
+        path_coords = np.array(path_coords)
+        plt.plot(path_coords[:, 0], path_coords[:, 1], color='blue', linewidth=2, label='Shortest Path')
+        
+        plt.scatter(path_coords[0, 0], path_coords[0, 1], color='green', label='Start Point')
+        plt.scatter(path_coords[-1, 0], path_coords[-1, 1], color='red', label='End Point')
+
+        plt.title('Shortest Path Inside Polygon')
+        plt.xlabel('Longitude')
+        plt.ylabel('Latitude')
+        plt.legend()
+        plt.show()
+
+    def smooth_trajectory(self, path_coords, num_points=100):
+        path_coords = np.array(path_coords)
+        x = path_coords[:, 0]
+        y = path_coords[:, 1]
+        t = np.linspace(0, 1, len(x))
+        
+        # Interpolation
+        x_interp = np.interp(np.linspace(0, 1, num_points), t, x)
+        y_interp = np.interp(np.linspace(0, 1, num_points), t, y)
+        
+        smooth_path_coords = list(zip(x_interp, y_interp))
+        return smooth_path_coords
 
 # Path to the OSM file
 osm_file_path = os.path.join(os.getcwd(), 'control', 'lanelet2_map_versimpeld.osm')
 
 point = (5.513063, 52.459907)
-# point = (10.46625, 55.382)
 
-map = Map(point, osm_file_path)
-map.plot_location()
+map_instance = Map(point, osm_file_path)
+# map_instance.plot_location()
+map_instance.discretize_polygon()
+# map_instance.plot_discretized_points()
 
 
-print(map.check_point_in_polygon(point))
-point2 = (5.511368, 52.458993)
-# point2 = (10.46805, 55.3815)
-print(map.check_point_in_polygon(point2))
-map.plot_location(point2)
+# print(map.check_point_in_polygon(point))
+# point2 = (5.511368, 52.458993)
+# print(map.check_point_in_polygon(point2))
+# map.plot_location(point2)
 # point3 = (5.511435, 52.459028)
 # print(map.check_point_in_polygon(point3))
 # map.plot_location(point3)
+
+# Define start and end positions
+start_position = (5.513063, 52.459907)
+end_position = (5.511204, 52.458896)
+
+# if map_instance.check_point_in_polygon(end_position):
+
+# Find the shortest path
+path_coords = map_instance.find_shortest_path(start_position, end_position)
+
+# Plot the shortest path
+map_instance.plot_path(path_coords)
+
+# Smooth the trajectory
+smooth_path_coords = map_instance.smooth_trajectory(path_coords)
+
+# Plot the smoothed trajectory
+map_instance.plot_path(smooth_path_coords)
+
+
 
